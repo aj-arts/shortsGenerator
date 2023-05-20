618139bb12420e14a509b8b98a937c6a3c67e35d
Ajinkya Gokule <ajinkyagokule@gmail.com>
2023-05-20 14:10:33 -0700

    changed the formatting of the text

--- editor.py
+++ editor.py
@@ -1,6 +1,6 @@
 from moviepy.editor import *
 
-filename = "video3.mp4"
+filename = "video2.mp4"
 audioDuration = 40
 partitions = 8
 captionString = "Richard Feynman said, “Never confuse education with intelligence, you can have a PhD and still be an idiot.” What are some real life examples of this? My professor, a brilliant neurosurgeon, once decided to directly smell a bottle of ammonia. He then told me “don't smell that”. I did not plan to! Me. Masters in cybersecurity and can't help my 5th grader with his math homework. My ex wife with a PhD in neuroscience driving my car around with the handbrake on calling me to ask about the noise and smell. I had a professor for higher mathematics who had real difficulties figuring out how to extract a cup of coffee from the vending machine. Bless him. I work with medical doctors all the time for work. Doctors are some of the dumbest smart people I have ever met."
@@ -24,7 +24,7 @@ def textClipArray(captionString, partitions):
             method='caption',
             size=(clip.w * 0.9,None),
             align='center',
-            font='Comic-Sans-MS-Bold',
+            font='Verdana-Bold',
             fontsize=clip.h * 0.025,
             bg_color='transparent',
             ).on_color(color=(0,0,0), col_opacity=0.8).set_duration(audioDuration/partitions).set_start(i*audioDuration/partitions).set_pos((0.05,0.15), relative=True))
