0ec9de9f884c00615511cebfb797819a400fa3b7
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2023-05-20 14:32:58 -0700

    added documentation to video.py

--- generator.py
+++ generator.py
@@ -1,10 +1,17 @@
 from videoScraper.video import Video
+from editor import videoEditor
 
 def main():
     # get video type from user
     videoType = input("Enter desired background video type: ")
     video = Video(videoType)
     video.saveVideo()
+
+    # apply caption to video
+    inputVideo = video.filename
+    savedVideo = videoEditor(inputVideo, captionString, audioFile)
+
+    print("Video saved as:", savedVideo, "in captionVideos folder")
 
 if __name__ == "__main__":
     main()
